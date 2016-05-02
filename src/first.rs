015b8013fa8cc1e17f9f34534ba320fc00eed2b9
Matt Pindell <pindell.m@gmail.com>
2016-05-02 17:08:10 -0400

    Experiment with impl blocks

---
+++ src/first.rs
@@ -0,0 +1,20 @@
+
+pub struct List {
+  head: Link,
+}
+
+enum Link {
+  Empty,
+  More(Box<Node>),
+}
+
+struct Node {
+  elem: i32,
+  next: List,
+}
+
+impl List {
+  pub fn new() -> Self {
+    List { head: Link::Empty }
+  }
+}
---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+pub mod first;
+
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn it_works() {
+    }
+}
