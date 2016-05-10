ee007e789bf3759e7a0151eb937ebe6819aa0c36
Matt Pindell <pindell.m@gmail.com>
2016-05-09 21:24:15 -0600

    Refactor Link to utilize Option and pop function to use .map

--- src/second.rs
+++ src/second.rs
@@ -4,10 +4,7 @@ pub struct List {
   head: Link,
 }
 
-enum Link {
-  Empty,
-  More(Box<Node>),
-}
+type Link = Option<Box<Node>>;
 
 struct Node {
   elem: i32,
@@ -16,35 +13,32 @@ struct Node {
 
 impl List {
     pub fn new() -> Self {
-        List { head: Link::Empty }
+        List { head: None }
     }
 
     pub fn push(&mut self, elem: i32) {
         let new_node = Box::new(Node {
             elem: elem,
-            next: mem::replace(&mut self.head, Link::Empty),
+            next: self.head.take(),
         });
 
-        self.head = Link::More(new_node);
+        self.head = Some(new_node);
     }
 
     pub fn pop(&mut self) -> Option<i32> {
-        match mem::replace(&mut self.head, Link::Empty) {
-            Link::Empty => None,
-            Link::More(boxed_node) => {
-                let node  = *boxed_node;
-                self.head = node.next;
-                Some(node.elem)
-            }
-        }
+        self.head.take().map(|node| {
+            let node  = *node;
+            self.head = node.next;
+            node.elem
+        })
     }
 }
 
 impl Drop for List {
     fn drop(&mut self) {
-        let mut cur_link = mem::replace(&mut self.head, Link::Empty);
-        while let Link::More(mut boxed_node) = cur_link {
-            cur_link = mem::replace(&mut boxed_node.next, Link::Empty)
+        let mut cur_link = self.head.take();
+        while let Some(mut boxed_node) = cur_link {
+            cur_link = mem::replace(&mut boxed_node.next, None)
         }
     }
 }
