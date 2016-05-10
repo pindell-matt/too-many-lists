e4a2627f3418fc86a09f86119c7ddf56567146d9
Matt Pindell <pindell.m@gmail.com>
2016-05-09 20:52:25 -0600

    Add corrected push function

--- src/first.rs
+++ src/first.rs
@@ -1,4 +1,4 @@
-user std::mem;
+use std::mem;
 
 pub struct List {
   head: Link,
@@ -11,20 +11,20 @@ enum Link {
 
 struct Node {
   elem: i32,
-  next: List,
+  next: Link,
 }
 
 impl List {
-  pub fn new() -> Self {
-    List { head: Link::Empty }
-  };
+    pub fn new() -> Self {
+        List { head: Link::Empty }
+    }
 
-  pub fn push(&mut self, elem: i32) {
-    let new_node = Box::new(Node {
-      elem: elem,
-      next: mem::replace(&mut self.head, Link::Empty),
-    });
+    pub fn push(&mut self, elem: i32) {
+        let new_node = Box::new(Node {
+            elem: elem,
+            next: mem::replace(&mut self.head, Link::Empty),
+        });
 
-    self.head = Link::More(new_node);
-  }
+        self.head = Link::More(new_node);
+    }
 }
